e1782491d9b8fcbce1d8cd53d20a6c15500c2e7f
jaysson18 <jahaka999@gmail.com>
2025-03-11 10:36:12 +0200

    Enhance media display in Single page and update MediaList to link to individual media items

--- src/components/MedialList.tsx
+++ src/components/MedialList.tsx
@@ -1,5 +1,6 @@
 import { fetchAllMedia } from "@/models/mediaModel";
 import Image from "next/image";
+import Link from "next/link";
 const MediaList = async () => {
   const mediaList = await fetchAllMedia();
 
@@ -15,16 +16,20 @@ const MediaList = async () => {
             key={index}
             className="flex flex-col items-center border border-gray-300 p-4 shadow-lg rounded-md bg-white"
           >
-            <Image
-              src={item.thumbnail}
-              alt={item.title}
-              className="h-[320]px w-[320px] object-cover rounded-md"
-              width={320}
-              height={320}
-            />
-            <h3 className="text-lg font-bold self-start">{item.title}</h3>
-            <p>Description: {item.description}</p>
-            <p>Date: {new Date(item.created_at).toLocaleDateString("fi-FI")}</p>
+            <Link href={"/single/" + item.media_id}>
+              <h3 className="text-lg font-bold self-start">{item.title}</h3>
+              <Image
+                src={item.thumbnail}
+                alt={item.title}
+                className="h-[320]px w-[320px] object-cover rounded-md"
+                width={320}
+                height={320}
+              />
+              <p>Description: {item.description}</p>
+              <p>
+                Date: {new Date(item.created_at).toLocaleDateString("fi-FI")}
+              </p>
+            </Link>
           </li>
         ))}
       </ul>
